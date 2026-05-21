b13810008f05803fdb110d81c32d0bd5c9cad6c9
shruti4265 <shrutisingh0735@gmail.com>
2026-05-21 23:42:57 +0530

    Handled db.js

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 import pg from "pg";
 import "dotenv/config";
-const db=new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-db.connect();
+
+const db = new pg.Pool(
+    process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+          }
+        : {
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+          }
+);
+
 export default db;
